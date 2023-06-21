3c70aa9b27b9097fff405f7e9ae24fc09cfc4f0e
Christina Lambiase <christina.lambiase@gmail.com>
2023-06-20 18:31:31 -0700

    completed all challenges

--- index.js
+++ index.js
@@ -23,6 +23,16 @@
 //Then I need to:
 //[x] add an event listener to the span that when you click, calls on renderDish and passes in the menu object
 
+//Challenge #4
+//The user should be able to add the menu items to their cart. When the user presses the 'Add to Cart' button, that number should be added to however many are currently in the cart.
+//For example, if there are currently 2 burritos in the cart, and the user adds 2 more, the total should be 4.
+//It is okay if the value resets in-between clicking on the menu items! It does not need to save state!
+
+//Lastly, I need to:
+//[x] declare the form, form input, and span number in cart variables in global scope (declare span as let since the value will change!)
+//[x] add an event listener to the *form* that when you submit, declares a new variable equal to the integer value of what you submitted, then adds that to the integer value of the current text content to make it the new text content of the span variable
+//[x] make sure you prevent default and reset the form in the event listener
+
 
 function returnMenu(url) {
     return fetch(url)
@@ -41,7 +51,9 @@ const img = document.querySelector("#dish-image")
 const dishName = document.querySelector("#dish-name")
 const dishDescr = document.querySelector("#dish-description")
 const dishPrice = document.querySelector("#dish-price")
-
+const cartForm = document.querySelector('#cart-form')
+const cartInput = document.querySelector('#cart-amount')
+let numInCart = document.querySelector('#number-in-cart')
 
 function renderMenu(menu) {
     const span = document.createElement('span')
@@ -56,6 +68,19 @@ function renderDish(menu) {
     dishName.textContent = menu.name
     dishDescr.textContent = menu.description
     dishPrice.textContent = menu.price
+    numInCart.textContent = menu.number_in_bag
 }
+
+//This is the part I need to really focus on!
+
+cartForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const newCart = parseInt(cartInput.value)
+    numInCart.textContent = parseInt(numInCart.textContent) + newCart
+    e.target.reset()
+})
+
+//Bonus Challenge #1
+//The value of the cart for each item should persist when clicking through the various menu items.
 
 
